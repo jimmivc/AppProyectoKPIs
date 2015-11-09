bbc331c6414e089ec8899c123595d07b69b67086
hsaenz <19hsp91@gmail.com>
2015-11-09 14:37:14 -0600

    avance iniciar sesion, aun no recupera usuario por correo

--- AppProyectoKPI/AppProyectoKPI/BL/loginController.vb
+++ AppProyectoKPI/AppProyectoKPI/BL/loginController.vb
@@ -15,4 +15,25 @@
 
     End Function
 
+    Shared Function iniciarSesion(pCorreo As String, pPass As String) As Usuario
+
+        Dim enc As Encrypt = New Encrypt(pPass)
+        Dim pass = enc.EncryptData(pPass)
+
+        Try
+            Dim user As Usuario = UsuariosBL.consultarUsuarioCorreo(pCorreo)
+            If user.Contrasena.Equals(pass) Then
+
+                Return user
+
+            End If
+
+        Catch ex As Exception
+            Throw
+        End Try
+
+        Return Nothing
+
+    End Function
+
 End Class
