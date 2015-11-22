9c83a19b1ba5df923eb3fb126507df6d161bf829
marJorieNav <marjorieanavarro@gmail.com>
2015-11-22 03:26:35 -0600

    caso de uso: registrar venta

---
+++ AppProyectoKPI/AppProyectoKPI/BL/VentaBL.vb
@@ -0,0 +1,39 @@
+﻿
+
+Imports System.Configuration
+    Imports System.Net
+    Imports Newtonsoft.Json
+    Imports RestSharp
+
+
+
+    Public Class VentaBL
+
+        Private Shared prospecto As Prospecto
+        Private Shared usuario As Usuario
+
+
+        Shared Function registrarVenta(ByVal fecha As DateTime, ByVal periodo As String, ByVal categoria As String, ByVal descripcion As String, ByVal monto As Double, prospecto As Prospecto) As Boolean
+            Dim client = New RestClient(ConfigurationManager.AppSettings.Get("endpoint"))
+            Dim request = New RestRequest("Ventas", Method.POST)
+            request.RequestFormat = DataFormat.Json
+
+            Dim venta As New Venta(0, fecha, periodo, categoria, descripcion, monto, prospecto, Nothing)
+
+            request.AddJsonBody(venta)
+
+            Dim response = client.Execute(request)
+
+            If (response.StatusCode.Equals(HttpStatusCode.OK)) Then
+                Return True
+            Else
+                Return False
+            End If
+
+            Return True
+        End Function
+
+    End Class
+
+
+
---
+++ AppProyectoKPI/AppProyectoKPI/RegistrarVenta.vb
@@ -0,0 +1,134 @@
+﻿
+
+Imports System.Configuration
+    Imports RestSharp
+    Imports System.Net
+    Imports System.IO
+
+
+
+
+Public Class RegistrarVenta
+
+
+
+    Private Sub BtnRegistrarVenta_Click(sender As Object, e As EventArgs) Handles BtnRegistrarVenta.Click
+
+        Dim fecha = textFecha.Text
+        Dim periodo = ComboBoxPeriodo
+        Dim categoria = ComboCategoria
+        Dim descripcion = TextDescripcion.Text
+        Dim monto = TextMonto.Text
+
+
+
+
+        If (textFecha.Text = "" And TextDescripcion.Text = "" And TextMonto.Text = "" And TextBuscar.Text = "" And TextIdMercadeo.Text = "") Then
+            MsgBox("Debe llegar el registro")
+
+        Else
+
+            'VALIDACION DE CAMPOS 
+
+            If (descripcion) = "" Then
+                MsgBox("Llenar campos")
+
+            ElseIf IsNumeric(descripcion) Then
+                MsgBox("solo acepta caracteres")
+            Else
+
+                If (monto) = "" Then
+
+                    MsgBox("ingrese datos validos, numericos")
+
+                Else
+
+                    Dim cliente As New Prospecto()
+
+
+
+                    cliente.Identificacion = TextBuscar.Text
+
+
+                    VentaBL.registrarVenta(textFecha.Text, ComboBoxPeriodo.Text, ComboCategoria.Text, descripcion, monto, cliente)
+                    MsgBox("Se registro correctamente")
+
+                    Dim venta As New Venta
+                    venta.Fecha = textFecha.Text
+                    venta.Periodo = periodo.Text
+                    venta.Categoria = categoria.Text
+                    venta.Descripcion = descripcion
+                    venta.Monto = monto
+
+
+                End If
+            End If
+        End If
+
+
+    End Sub
+
+
+    Private Sub BuscarCliente_Click(sender As Object, e As EventArgs) Handles BuscarCliente.Click
+
+
+
+        If (TextBuscar.Text = "") Then
+
+            MsgBox("Ingrese el numero de identificacion")
+        Else
+
+            CargaBusquedaProspecto_Load(TextBuscar.Text)
+
+        End If
+
+
+    End Sub
+    Private Sub CargaBusquedaProspecto_Load(ByVal identificacion As String)
+
+
+
+
+        Dim response = ProspectoBL.ObtenerProspectoXIdentificacion(identificacion)
+
+        If (response Is Nothing) Then
+            MsgBox("no se encontro prospecto")
+        Else
+
+            Dim usuario As New Usuario()
+
+
+
+            'Me.TextDescripcion.Text = response.Identificacion
+            Me.TextIdMercadeo.Text = response.Nombre
+            Me.TextVendedor.Text = response.Usuario.Nombre
+
+
+
+        End If
+
+    End Sub
+
+
+    Private Sub TextMonto_KeyPress(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles TextMonto.KeyPress
+        Validaciones.VerificarNumeros(e)
+
+    End Sub
+
+
+    Private Sub BtnCancelar_Click(sender As Object, e As EventArgs) Handles BtnCancelar.Click
+
+
+        TextDescripcion.Text = ""
+        TextMonto.Text = ""
+        TextBuscar.Text = ""
+        TextIdMercadeo.Text = ""
+        TextVendedor.Text = ""
+
+
+
+    End Sub
+
+
+End Class
+
