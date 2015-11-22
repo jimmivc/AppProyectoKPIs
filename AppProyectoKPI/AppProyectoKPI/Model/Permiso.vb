2e8d31f8ef3edf29b1ace1ca78e5b1403110bd6f
hsaenz <19hsp91@gmail.com>
2015-11-22 15:28:38 -0600

    11/22 APP Hernan

--- AppProyectoKPI/AppProyectoKPI/Model/Permiso.vb
+++ AppProyectoKPI/AppProyectoKPI/Model/Permiso.vb
@@ -5,12 +5,20 @@ Public Class Permiso
     Private _idPermiso As Integer
     Private _descripcion As String
     Private _accion As String
+    Private _roles As List(Of Rol)
 
     Private Sub New()
 
     End Sub
 
-    Public Property IdPermiso As Integer
+    Public Sub New(pId As Integer, pDesc As String, pAccion As String, pRoles As List(Of Rol))
+        PermisoID = pId
+        Descripcion = pDesc
+        Accion = pAccion
+        Roles = pRoles
+    End Sub
+
+    Public Property PermisoID As Integer
         Get
             Return _idPermiso
         End Get
@@ -34,6 +42,15 @@ Public Class Permiso
         End Get
         Set(value As String)
             _accion = value
+        End Set
+    End Property
+
+    Public Property Roles As List(Of Rol)
+        Get
+            Return _roles
+        End Get
+        Set(value As List(Of Rol))
+            _roles = value
         End Set
     End Property
 End Class
