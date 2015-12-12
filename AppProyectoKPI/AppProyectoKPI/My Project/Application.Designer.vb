86fd88f4297da60dc19bb2549c1d75e195d92f74
jimmi <jimmivco@hotmail.com>
2015-12-12 01:37:26 -0600

    pantalla principal

--- AppProyectoKPI/AppProyectoKPI/My Project/Application.Designer.vb
+++ AppProyectoKPI/AppProyectoKPI/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.AppProyectoKPI.AsignacionProspectos
+            Me.MainForm = Global.AppProyectoKPI.Principal
         End Sub
     End Class
 End Namespace
